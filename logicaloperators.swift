6f9a3d8b80a9d7d782573755edfdf87a2bafe407
Lee Len Formento <leebatef@gmail.com>
2019-02-25 07:39:23 -0500

    Logical operators

---
+++ logicaloperators.swift
@@ -0,0 +1,34 @@
+// ======== Logical Operators ===========
+
+// For each of the logical expressions below, print out what you think the resulting value will be (true or false). Then print out the actual expression to see if you were right. An example has been provided below.
+
+ 15 == 15
+ print(true)
+ print( 15 == 15)
+
+ 42 > 90
+ print(false)
+ print(42 > 90)
+
+ 42 < 90
+ print(true)
+ print( 42 < 90)
+
+ 84 != 84
+ print(false)
+ print(84 != 84)
+
+ 4 <= 4
+ print(true)
+ print(4 <= 4) 
+
+ (42 > 88) && ( 42 < 88)
+ print(false)
+print((42 > 88) && ( 42 < 88))
+
+(42 > 88) || (42 < 88)
+ print(true)
+ print((42 > 88) || ( 42 < 88))
+
+!true
+print(false)
